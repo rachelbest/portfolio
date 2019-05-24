8a08ab9fededa14b4a320f42dcccfa353a4fa1d1
rachelbtech <rachelb@mumsnet.com>
2019-05-24 17:36:59 +0100

    Updates

---
+++ precache-manifest.31d07fcc6aca23a6e8e7c0279c21e043.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb7d28e39e83f9a27e8c55c2fa1166b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81169abd8611c4e8e3b4",
+    "url": "/static/css/main.1ca952dc.chunk.css"
+  },
+  {
+    "revision": "01785baa88a55ddbe39b",
+    "url": "/static/js/2.0f307307.chunk.js"
+  },
+  {
+    "revision": "81169abd8611c4e8e3b4",
+    "url": "/static/js/main.7de9730a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e561a2486939da58bde227c1d97d6cf1",
+    "url": "/static/media/brian.e561a248.png"
+  },
+  {
+    "revision": "492b8d3b67a5d1d01f45c7f11a81a083",
+    "url": "/static/media/lego.492b8d3b.png"
+  },
+  {
+    "revision": "7e8c36eaba5e368d9e34b2cff51fa37c",
+    "url": "/static/media/mike.7e8c36ea.png"
+  },
+  {
+    "revision": "175d281f5d93ed62707edf8d9c3fd538",
+    "url": "/static/media/minion.175d281f.png"
+  },
+  {
+    "revision": "7ae54d146551375805b474ad0dba7197",
+    "url": "/static/media/rb-logo-circled.7ae54d14.png"
+  },
+  {
+    "revision": "0fa8f0c0530611350d7b666663812f64",
+    "url": "/static/media/spongebob.0fa8f0c0.png"
+  },
+  {
+    "revision": "e0769aff9f88673e17934cc6fc613e98",
+    "url": "/static/media/stewie.e0769aff.png"
+  }
+]);
