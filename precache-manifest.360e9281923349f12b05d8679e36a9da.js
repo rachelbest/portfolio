f11fa20fffb0c9bf711abaded8be0d703309ea6e
rachel13bull <rachel13bull@gmail.com>
2019-05-26 10:05:58 +0100

    Updates

---
+++ precache-manifest.360e9281923349f12b05d8679e36a9da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381fb9e46970b243426e3a2010eb4706",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "75126dac773741e6d5a7",
+    "url": "/portfolio/static/css/main.0887eee1.chunk.css"
+  },
+  {
+    "revision": "01785baa88a55ddbe39b",
+    "url": "/portfolio/static/js/2.0f307307.chunk.js"
+  },
+  {
+    "revision": "75126dac773741e6d5a7",
+    "url": "/portfolio/static/js/main.224f49b3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e561a2486939da58bde227c1d97d6cf1",
+    "url": "/portfolio/static/media/brian.e561a248.png"
+  },
+  {
+    "revision": "492b8d3b67a5d1d01f45c7f11a81a083",
+    "url": "/portfolio/static/media/lego.492b8d3b.png"
+  },
+  {
+    "revision": "7e8c36eaba5e368d9e34b2cff51fa37c",
+    "url": "/portfolio/static/media/mike.7e8c36ea.png"
+  },
+  {
+    "revision": "175d281f5d93ed62707edf8d9c3fd538",
+    "url": "/portfolio/static/media/minion.175d281f.png"
+  },
+  {
+    "revision": "7ae54d146551375805b474ad0dba7197",
+    "url": "/portfolio/static/media/rb-logo-circled.7ae54d14.png"
+  },
+  {
+    "revision": "0fa8f0c0530611350d7b666663812f64",
+    "url": "/portfolio/static/media/spongebob.0fa8f0c0.png"
+  },
+  {
+    "revision": "e0769aff9f88673e17934cc6fc613e98",
+    "url": "/portfolio/static/media/stewie.e0769aff.png"
+  }
+]);
