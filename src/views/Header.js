140ea393d958b329b9ae5e47bbc0d1d7644f9654
rachel13bull <rachel13bull@gmail.com>
2020-08-13 14:18:39 +0100

    Make header animated and fixed on scroll

--- src/views/Header.js
+++ src/views/Header.js
@@ -5,12 +5,29 @@ import logo from './../assets/images/rb-logo-circled.png';
 import './../assets/css/header.css';
 
 export default class Header extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { scrollClass: "header__absolute" }
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', () => {
+			let isTop = window.scrollY < 40;
+			if (isTop !== true) {
+				this.setState({ scrollClass: "header__sticky" })
+			} else {
+				this.setState({ scrollClass: "header__absolute" })
+			}
+		});
+	}
 
 	render() {
+		let { scrollClass } = this.state
+
 		return (
-			<header className="header">
+			<header className={"header " + scrollClass}>
 				<div className="header__container">
-					<NavLink to="/">
+					<NavLink to="/" className="header__logo">
 						<img src={logo} alt="logo" height="70"/>
 					</NavLink>
 					<nav>
