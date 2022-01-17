04b5d38b62e5b5e56a5b1b6ff0a2082d1e04c53c
rachel13bull <rachel13bull@gmail.com>
2022-01-17 13:49:40 +0000

    Experience update & tidy code

--- src/views/About.js
+++ src/views/About.js
@@ -8,9 +8,9 @@ export default class Landing extends Component {
 
 	render() {
 		return (
-			<section className="about-me">
+			<section className="aboutme">
 				<article>
-					<img className="profile" src={rachel} alt="Rachel Best Digital Art" width="200" height="auto"/>
+					<img className="aboutme__image" src={rachel} alt="Rachel Best Digital Art" width="200" height="auto"/>
 					<h1>Rachel Best</h1>
 					<p className="subheading">Frontend Web Developer</p>
 					<p>I am a Frontend Web Developer from London with 10+ years experience in coding responsive web solutions for multiple platforms. I have a First-Class degree in Multimedia Computing (BSc) from Anglia Ruskin University where I discovered my passion for creative web development.</p>
--- src/views/Experience.js
+++ src/views/Experience.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
 
+import mumsnet from './../assets/images/companies/mumsnet_logo.svg';
+import incentivated from './../assets/images/companies/incentivated_logo.png';
+import havillands from './../assets/images/companies/havillands_logo.jpg';
+
 export default class Experience extends Component {
 
 	render() {
 		return (
 			<section>
 				<h2>Experience</h2>
-				<div className="experience first">
-					<p className="small">Nov 2011 – Present</p>
-					<h3>Frontend Web Developer</h3>
-					<p className="subheading">Mumsnet</p>
-					<p>An enthusastic member of the team creating web templates and design solutions. Delivering high quality, user-friendly products across multiple platforms.</p>
+				<div className="experience">
+					<img className="experience__logo" src={mumsnet} alt="Mumsnet" height="50" width="160" />
+					<h3 className="experience__title">Frontend Web Developer</h3>
+					<p className="experience__subheading">Mumsnet <span>Nov 2011 – Present</span></p>
+					<p className="experience__intro">An long time team member, delivering responsive and accessible web solutions across multiple platforms for the most trusted, and most influencial website for parents in the UK.</p>
+					<ul>
+						<li>Contribute high quality, readable code across entire stack (500k+ lines of code)</li>
+						<li>Develop responsive & accessibile web content for cross-platform site with 1.2 billion page views per year</li>
+						<li>Contribute to two transformations of entire stack from page-based styles to modular component styles</li>
+						<li>Working with advertising brands to provide bespoke advertising solutions within tight deadlines</li>
+						<li>Lead the team with responsibility to ensure site meets web accessibility standards</li>
+						<li>Optimise site through A/B testing and personalisation using Google Optimize & Kameleoon</li>
+						<li>Competent peer reviews, quality checking & pair programming</li>
+						<li>Agile development & working closely with other teams</li>
+					</ul>
+					<p>JS, jQuery, AlpineJS, VueJS, CSS, Sass, Tailwind, Bootstrap, Laravel, Livewire & Haml</p>
 				</div>
 				<div className="experience">
-					<p className="small">July – Nov 2011</p>
-					<h3>Designer/Developer</h3>
-					<p className="subheading">Incentivated</p>
-					<p>Creating digital design solutions for web and mobile applications. Implementing designs and updating the company website.</p>
+					<img className="experience__logo" src={incentivated} alt="Incentivated" height="auto" width="160" />
+					<h3 className="experience__title">Designer/Developer</h3>
+					<p className="experience__subheading">Incentivated <span>July – Nov 2011</span></p>
+					<p className="experience__intro">Internship with a split role as a member of IT and Creative/Production teams, creating digital design solutions for mobile applications agency.</p>
+					<ul>
+						<li>Created animated banner ads, mock ups for approval & integrated CMS blog on company website</li>
+						<li>Email templates & newsletters for clients</li>
+						<li>Provided mock ups, graphs and assets for presentations, award entries and client pitches</li>
+						<li>Clients whose accounts were worked on included British Airways, Waitrose, PwC, Scottish Power, Jameson & British Heart Foundation</li>
+					</ul>
+					<p>HTML, CSS & jQuery</p>
 				</div>
 				<div className="experience">
-					<p className="small">Jan – Mar 2011</p>
-					<h3>Trainee Designer/Developer</h3>
-					<p className="subheading">Havillands & Co.</p>
-					<p>Designing and implementing several websites for SEO purposes using HTML, CSS, PHP and basic JavaScript.</p>
+					<img className="experience__logo" src={havillands} alt="Havillands & Co" height="auto" width="160" />
+					<h3 className="experience__title">Trainee Designer/Developer</h3>
+					<p className="experience__subheading">Havillands & Co. <span>Jan – Mar 2011</span></p>
+					<p className="experience__intro">Part-time role while studying for university degree. Designing and implementing several websites for SEO purposes.</p>
+					<ul>
+						<li>Created several affiliate website with tight deadlines</li>
+						<li>Worked alongside senior developers to learn new skills</li>
+						<li>Utilized Google tools to improve SEO</li>
+					</ul>
+					<p>HTML, CSS, & JS</p>
 				</div>
 			</section>
 		)
